f4ff326ab795c0a85249fd3b918a3db719489eb8
Nik <nik@iconluxurygroup.com>
2024-08-12 16:18:26 -0400

    image url fixed burberry

--- site_parsing_htmls/Agent_Node/main.py
+++ site_parsing_htmls/Agent_Node/main.py
@@ -1337,8 +1337,8 @@ class BurberryProductParser(WebsiteParser):
 
             discount_price = ""  # empty for now and can be added later if found
 
-            image_url = product.select_one(
-                '.redesigned-product-card__picture img, .product-card-v2-carousel-container__media__picture img')['src']
+            image_url = product.select_one('.red    esigned-product-card__picture img, .product-card-v2-carousel-container__media__picture img')
+            image_url=image_url['src'] if image_url else ""
 
             tags = product.select_one('.product-card-labels__flag, .product-card-v2-carousel-labels__label')
             tags = tags.get_text(strip=True) if tags else ""
