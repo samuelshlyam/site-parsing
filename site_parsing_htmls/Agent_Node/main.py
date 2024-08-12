f10988b2b6f0e257b55f541b7e9d0c9f5c6ab5fc
Nik <nik@iconluxurygroup.com>
2024-08-12 13:33:55 -0400

    prod ready

--- site_parsing_htmls/Agent_Node/main.py
+++ site_parsing_htmls/Agent_Node/main.py
@@ -3165,4 +3165,4 @@ async def brand_batch_endpoint(job_id:str, brand_id: str, scan_url:str, backgrou
 
     return {"message": "Notification sent in the background"}
 if __name__ == "__main__":
-    uvicorn.run("main_parser:app", port=8004, log_level="info")
+    uvicorn.run("main:app", port=8080,host='0.0.0.0' log_level="info")
